0aba24fda6ff8cb0856547c760c0c734aca68aff
Karthi <karthikthee7@gmail.com>
2024-08-06 19:44:58 +0530

    constants update

--- app/constant.ts
+++ app/constant.ts
@@ -1,2 +1,9 @@
+//Prod
+
 export const SERVER_API_URL = "http://13.201.194.87:8000";
-// export const SERVER_API_URL = "http://localhost:8000"
+export const PDF_URL = "http://13.201.194.87"
+
+//Local
+
+// export const SERVER_API_URL = "http://localhost:8000";
+// export const PDF_URL = "http://localhost:3000";
