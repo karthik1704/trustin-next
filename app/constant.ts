02c563abbb89ccaa5104ee965bd55401352793d5
Karthi <karthikthee7@gmail.com>
2024-09-03 21:25:34 +0530

    cosnt

--- app/constant.ts
+++ app/constant.ts
@@ -3,7 +3,25 @@
 export const SERVER_API_URL = "https://libkesapi.vimkes.com";
 export const PDF_URL = "https://libkes.vimkes.com"
 
-// //Local
+// // //Local
 
 // export const SERVER_API_URL = "http://localhost:8000";
 // export const PDF_URL = "http://localhost:3000";
+
+
+export type StandardKey = 'IS' | 'ISO' | 'EN' | 'ASTM' | 'AATCC' | 'BP' | 'IP' | 'USP';
+
+type Standards = {
+    [key in StandardKey]: string;
+};
+
+export const standards:Standards = {
+    IS: "Indian Standard",
+    ISO: "International Organization for Standardization",
+    EN: "European Standards",
+    ASTM: "American Society for Testing and Materials",
+    AATCC: "American Association of Textile Chemists and Colorists",
+    BP: "British Pharmacopoeia",
+    IP: "Indian Pharmacopoeia",
+    USP: "United States Pharmacopeia"
+};
